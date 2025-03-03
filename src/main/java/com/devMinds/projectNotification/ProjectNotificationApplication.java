0df365cc5d03761f46c928951cc35be8b3c5a872
SABAREESWARAN12 <sabareessn12@gmail.com>
2025-03-03 12:06:09 +0530

    first commit

---
+++ src/main/java/com/devMinds/projectNotification/ProjectNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.devMinds.projectNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devMinds/projectNotification/ProjectNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devMinds.projectNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
